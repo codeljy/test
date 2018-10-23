ad3c41ae39d6181542d9c5790a116f14e97ddee4
codeljy <codeljy@163.com>
2018-10-23 16:06:43 +0800

    1

--- src/main/java/top/codeljy/BackendTestApplication.java
+++ src/main/java/top/codeljy/BackendTestApplication.java
@@ -1,7 +1,5 @@
 package top.codeljy;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
